0b2e41571480c6ad86095eefa207048df1cfabd9
Nazmi Altun <nazmialtun@windowslive.com>
2020-03-19 18:32:15 +0800

    baby steps

--- src/components/InputField/component.tsx
+++ src/components/InputField/component.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
+import * as _ from 'lodash';
 import { Testable } from '../../modules/testing';
+import { Input, TextArea } from './styled';
 
 export type Props = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'defaultValue' | 'value'> &
   Testable & {
     label: string;
-    value?: unknown;
+    value?: string | string[] | number;
+    rows?: number;
     onChange?: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
   };
 
 export const component = (props: Props) => {
-  return <div>{props.label}</div>;
+  let innerComponent;
+
+  if (!_.isUndefined(props.rows) && props.rows > 1) {
+    innerComponent = <TextArea rows={props.rows} value={props.value} />;
+  } else {
+    innerComponent = <Input value={props.value} />;
+  }
+
+  return <div>{innerComponent}</div>;
 };
 
 component.displayName = 'InputField';
