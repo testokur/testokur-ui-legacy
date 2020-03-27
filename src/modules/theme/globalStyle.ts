1f4926670bc7f405b6129856786e9491a4cb652f
Nazmi Altun <nazmialtun@windowslive.com>
2020-03-27 17:04:43 +0800

    linting issue fixed

--- src/modules/theme/globalStyle.ts
+++ src/modules/theme/globalStyle.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 const globalStyle = createGlobalStyle`
     :root{
-        background: ${({ theme }) => theme.colors.background};
+        background: ${({ theme }): string => theme.colors.background};
     }
 `;
 
--- src/modules/theme/themes.d.ts
+++ src/modules/theme/themes.d.ts
@@ -2,5 +2,5 @@ import 'styled-components';
 import { Theme } from '.';
 
 declare module 'styled-components' {
-  export interface DefaultTheme extends Theme {}
+  export interface DefaultTheme extends Theme {} // eslint-disable-line @typescript-eslint/no-empty-interface
 }
