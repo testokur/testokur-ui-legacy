77b1b82580c74b7b427660bb1a552efa75a9a239
Nazmi Altun <nazmialtun@windowslive.com>
2020-04-14 15:01:02 +0800

    more stories

--- src/components/Button/component.stories.tsx
+++ src/components/Button/component.stories.tsx
@@ -5,7 +5,7 @@ import { text, select } from '@storybook/addon-knobs';
 import Button from '.';
 import { Sizes } from '../../modules';
 import { Types } from './const';
-import { Remove } from '../Icon';
+import { Remove, PlusCircle } from '../Icon';
 
 storiesOf('Button', module)
   .add('Default', () => {
@@ -22,4 +22,15 @@ storiesOf('Button', module)
     const type = select('Type', Object.values(Types), Types.Primary);
 
     return <Button onClick={action('clicked')} size={size} buttonType={type} iconLeft={<Remove />} circled title="Button" />;
+  })
+  .add('Button with icon', () => {
+    const size = select('Size', Object.values(Sizes), Sizes.Medium);
+    const type = select('Type', Object.values(Types), Types.Primary);
+    const children = text('Children', 'Add');
+
+    return (
+      <Button onClick={action('clicked')} size={size} buttonType={type} iconLeft={<PlusCircle />} title="Add">
+        <>{children}</>
+      </Button>
+    );
   });
