021614d523e996859d67e1597fd6ef41da152d77
Nazmi Altun <nazmialtun@windowslive.com>
2020-04-14 16:19:24 +0800

    webpack config added

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        extractComments: true,
+      }),
+    ],
+  },
+};
