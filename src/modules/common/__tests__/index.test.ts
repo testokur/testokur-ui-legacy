62290b54de50a7c9f42c4df9c883e5b6a6b03942
Nazmi Altun <nazmialtun@windowslive.com>
2020-05-07 13:44:32 +0800

    tests for common module

---
+++ src/modules/common/__tests__/index.test.ts
@@ -0,0 +1,32 @@
+import { getSpacing, SpacingsAfter, getColor, Colors, Sizes, getSize } from '..';
+import { createTheme } from '../../theme';
+
+const isArrayUnique = (arr: string[]): boolean => new Set(arr).size === arr.length;
+const theme = createTheme({});
+
+describe('getSpacing', () => {
+  it('should return spacing', () => {
+    const spacings: string[] = [];
+    Object.keys(SpacingsAfter).map(x => spacings.push(getSpacing(theme)(x)));
+    expect(isArrayUnique(spacings)).toBeTruthy();
+    expect(spacings).toHaveLength(Object.keys(SpacingsAfter).length);
+  });
+});
+
+describe('getColor', () => {
+  it('should return colors', () => {
+    const colors: string[] = [];
+    Object.keys(Colors).map(x => colors.push(getColor(theme)(x)));
+    expect(isArrayUnique(colors)).toBeTruthy();
+    expect(colors).toHaveLength(Object.keys(Colors).length);
+  });
+});
+
+describe('getSize', () => {
+  it('should return sizes', () => {
+    const sizes: string[] = [];
+    Object.keys(Sizes).map(x => sizes.push(getSize(theme)(x)));
+    expect(isArrayUnique(sizes)).toBeTruthy();
+    expect(sizes).toHaveLength(Object.keys(Sizes).length);
+  });
+});
