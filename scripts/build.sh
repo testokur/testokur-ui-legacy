20298fc920b612035cb71df3025d4f3f26e3b3f3
Nazmi Altun <nazmialtun@windowslive.com>
2020-05-12 03:15:12 +0800

    fix: build process

---
+++ scripts/build.sh
@@ -0,0 +1,2 @@
+yarn run tsc --declaration true --declarationMap true --module esnext --outDir "./dist/es"
+yarn run tsc --declaration true --declarationMap true --module commonjs --outDir "./dist/commonjs"
