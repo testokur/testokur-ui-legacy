ac9bfafd0343aadea39fb1710065c84c2ac41551
Nazmi Altun <nazmialtun@windowslive.com>
2020-05-12 03:36:48 +0800

    fix : webpack removed from

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,3 @@
+rimraf dist storybook-static
 yarn run tsc --declaration true --declarationMap true --module esnext --outDir "./dist/es"
 yarn run tsc --declaration true --declarationMap true --module commonjs --outDir "./dist/commonjs"
