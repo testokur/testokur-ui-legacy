b2b5354917bfeb773f998532cbeea8e1c1e945f9
Nazmi Altun <nazmialtun@windowslive.com>
2020-05-14 15:23:59 +0800

    fix: build process

--- scripts/build.sh
+++
@@ -1,3 +0,0 @@
-rimraf dist storybook-static
-yarn run tsc --declaration true --declarationMap true --module esnext --outDir "./dist/es"
-yarn run tsc --declaration true --declarationMap true --module commonjs --outDir "./dist/commonjs"
