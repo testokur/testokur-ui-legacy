b318453b9ca4bd5c45e73b4cba27f6bf2bcb9284
Nazmi Altun <nazmialtun@windowslive.com>
2020-06-01 18:56:28 +0800

    Still WIP : hide

--- src/components/Hide/getViewportHideStyles.ts
+++ src/components/Hide/getViewportHideStyles.ts
@@ -10,11 +10,12 @@ const getViewportHideStyles: (on: Devices[], block?: boolean) => FlattenInterpol
   const mapFunc: (viewport: string) => FlattenInterpolation<ThemeProps<DefaultTheme>> = (viewport: string) => {
     if (viewport in mediaQueries) {
       return css`
-        ${mediaQueries[viewport as keyof typeof mediaQueries](css``)};
-        display: ${on.indexOf(viewport as Devices) !== -1 ? 'none' : getBlockStyle()};
+        ${mediaQueries[viewport as keyof typeof mediaQueries](css`
+          display: ${on.indexOf(viewport as Devices) !== -1 ? 'none' : getBlockStyle()};
+        `)};
       `;
     }
-    if (viewport === 'smallMobile' && on.indexOf(viewport as Devices) !== -1) {
+    if (viewport === Devices.SmallMobile && on.indexOf(viewport as Devices) !== -1) {
       return css`
         display: none;
       `;
